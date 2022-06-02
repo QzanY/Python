7f107ff2f428c4b6d00637e4a6735e62e7552586
QzanY <93677118+QzanY@users.noreply.github.com>
2022-06-02 22:15:13 +0300

    Update customtkinter1.py.py

--- customtkinter1.py.py
+++ customtkinter1.py.py
@@ -1,6 +1,9 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import DISABLED, messagebox,filedialog
 import customtkinter as ct
+from docx import Document
+import pandas as pd
+
 
 ct.set_appearance_mode("Dark")
 ct.set_default_color_theme("blue")
@@ -18,10 +21,6 @@ class Window(ct.CTk):
         self.geometry(f"{Window.WIDTH}x{Window.HEIGHT}")
         # widget_width_multiplier = self.winfo_width()/self.winfo_height()
         
-        self.Ltext1 = "Merhaba"
-        self.name = "Ozan"
-        self.Ltext2 = f"Benim adim {self.name}"
-
         self.startUI()
         
     def startUI(self):
@@ -30,13 +29,21 @@ class Window(ct.CTk):
         self.main_Lframe.grid(row=0,column=0,rowspan=2,padx=10,pady=10)
         self.main_Lframe.grid_propagate(0)
 
-        # self.main_editLframe = ct.CTkFrame(self.main_Lframe,width=380,height=580)
-        # self.main_editLframe.grid(row=0,column=0,sticky="nsew")
-        self.main_Lframe.rowconfigure(0,weight=1)
+        self.main_Lframe.rowconfigure(0,minsize=20)
+        self.main_Lframe.rowconfigure(2,weight=1)
         self.main_Lframe.rowconfigure(4,weight=1)
-
         self.main_Lframe.columnconfigure(0,weight=1)
         self.main_Lframe.columnconfigure(2,weight=1)
+
+        self.main_labelLframe = ct.CTkFrame(self.main_Lframe,width=300,height=100)
+        self.main_labelLframe.grid_propagate(0)
+        self.main_labelLframe.rowconfigure(1,minsize=10)
+        self.main_labelLframe.rowconfigure(3,minsize=10)
+        self.main_labelLframe.columnconfigure(0,weight=1)
+        self.main_labelLframe.columnconfigure(2,weight=1)
+
+        self.main_labelLframe.grid(row=1,column=1,sticky="nsew")
+
 
         self.main_Rframe1 = ct.CTkFrame(self,width=380,height=285)
         self.main_Rframe1.grid(row=0,column=1)
@@ -53,17 +60,20 @@ class Window(ct.CTk):
         self.main_Rframe2 = ct.CTkFrame(self,width=380,height=285)
         self.main_Rframe2.grid(row=1,column=1)
         self.main_Rframe2.grid_propagate(0)
-        self.main_editRframe2 = ct.CTkFrame(self.main_Rframe2,width=380,height=285)
-        self.main_editRframe2.grid(row=0,column=0,sticky="nsew")
+        # self.main_labelRframe2 = ct.CTkFrame(self.main_Rframe2,width=380,height=285)
+        # self.main_labelRframe2.grid(row=0,column=0,sticky="nsew")
 
         #----------- Labels ----------------
         
-        self.Llabel1 = ct.CTkLabel(self.main_Lframe,text=self.Ltext1)
-        self.Llabel1.grid(row=1,column=1,sticky="nsew")
-        self.Llabel2 = ct.CTkLabel(self.main_Lframe,text=self.Ltext2)
+        self.Llabel1 = ct.CTkLabel(self.main_labelLframe,text="Merhaba")
+        self.Llabel1.grid(row=0,column=1,sticky="nsew")
+        self.Llabel2 = ct.CTkLabel(self.main_labelLframe,text="İsminizi bahşeder misiniz?")
         self.Llabel2.grid(row=2,column=1)
-        self.Llabel3 = ct.CTkLabel(self.main_Lframe,text="hahahahaaahahah")
-        self.Llabel3.grid(row=3,column=1)
+        self.Llabel3 = ct.CTkLabel(self.main_labelLframe,text="hahahahaaahahah")
+        self.Llabel3.grid(row=4,column=1)
+
+        # self.R2documentlabel = ct.CTkLabel(self.main_Rframe2,text="")
+        # self.R2documentlabel.grid()
 
         
         
@@ -73,30 +83,44 @@ class Window(ct.CTk):
         self.R1button1 = ct.CTkButton(self.main_Rframe1,text="Click Me!",command=self.namechange)
         self.R1button1.grid(row=1,column=1)
 
-        self.R1button2 = ct.CTkButton(self.main_Rframe1,text="Don't Click Me!",command=self.getname)
-        self.R1button2.grid(row=3,column=1)
+        self.Lbutton = ct.CTkButton(self.main_Lframe,text="Select document", command=self.select_document)
+        self.Lbutton.grid(row=3,column=1)
 
         
         #----------- Entries -----------------
         
         self.entvar1 = tk.StringVar(self,value="")
-        self.R2entry1 = ct.CTkEntry(self.main_editRframe2,textvariable=self.entvar1,width=150)
-        self.R2entry1.grid()
-
-        #----------- Textbox ----------------
-
-        self.R2textbox = tk.Text(self.main_Rframe2,bg="#3d3d3d",fg="white",font="Helvetica")
-        self.R2textbox.grid(row=1,column=0)
-
+        self.R2entry1 = ct.CTkEntry(self.main_Rframe1,textvariable=self.entvar1,width=150)
+        self.R2entry1.grid(row=3,column=1)
+        
 
     def namechange(self):
-        self.name = self.R2entry1.get()
-        self.Llabel2.destroy()
-        self.Llabel2 = ct.CTkLabel(self.main_Lframe,text=f"Benim adim {self.name}")
-        self.Llabel2.grid(row=2,column=1)
-        # pass
-    def getname(self):
-        pass
+        if len(self.R2entry1.get()) > 20:
+            messagebox.showerror("Long Name","The name you've written is too long to display. It's length must be 20 characters or less.")
+        elif self.R2entry1.get() == "":
+            self.Llabel2.config(text="İsminiz yok mu?")
+        else:
+            self.Llabel2.config(text=f"Benim adım {self.R2entry1.get()}")         # ÇOK ÖNEMLİ BİR ARAÇ
+
+    def select_document(self):
+        file = filedialog.askopenfilename(title="Select Your Document",initialdir="/home/bs2022/e2581163/Desktop")
+        try:
+            if file.split(".")[-1] == "docx":
+                self.editor = tk.Text(self.main_Rframe2,bg="#3d3d3d",fg="white")
+                self.editor.grid()
+                doc = Document(file)
+                text1 = doc.paragraphs[2].text
+                self.editor.insert(tk.END,text1)
+            elif file.split(".")[-1] == "xls":
+                table = pd.read_excel(file)
+                print(table)
+        except AttributeError:
+            pass
+
+
+
+
+    
 
 
 if __name__ == "__main__":
